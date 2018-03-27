e63f958ab8cfd1dcec2f7ed7f91a9aabdce45f78
Yassin Hassan <yh@yassinhassan.com>
2018-03-27 11:17:49 +0200

    Adding function getDecreasingAmountPerDay

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,12 +1,12 @@
 public class Product {
     private String name;
     private int shelflife; // in days
-    private double prive;
+    private double price;
 
-    public Product(String name, int shelflife, double prive) {
+    public Product(String name, int shelflife, double price) {
         this.name = name;
         this.shelflife = shelflife;
-        this.prive = prive;
+        this.price = price;
     }
 
     public String getName() {
@@ -25,11 +25,15 @@ public class Product {
         this.shelflife = shelflife;
     }
 
-    public double getPrive() {
-        return prive;
+    public double getPrice() {
+        return price;
     }
 
-    public void setPrive(double prive) {
-        this.prive = prive;
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDecreasingAmountPerDay(){
+        return this.price/this.shelflife;
     }
 }
--- src/test/java/TestProduct.java
+++ src/test/java/TestProduct.java
@@ -8,4 +8,11 @@ public class TestProduct {
         Product p = new Product("Milk", 5, 2.51);
         Assert.assertTrue(p != null);
     }
+
+    @Test
+    public void getDecreasingAmountPerDay() {
+        Product p = new Product("Kitkat", 10, 30);
+        double amount = p.getDecreasingAmountPerDay();
+        Assert.assertEquals("Decreasing amount must be 3",3.0, amount, 0);
+    }
 }
